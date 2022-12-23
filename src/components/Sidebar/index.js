502e2ae501350c6db36c1f74158159d47f654d04
Corey Gallagher <coreygallagher1194@gmail.com>
2022-12-23 12:23:18 -0600

    remove youtube and twitter

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,8 +6,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faLinkedin,
   faGithub,
-  faYoutube,
-  faSkype,
 } from '@fortawesome/free-brands-svg-icons'
 import {
   faHome,
@@ -72,7 +70,7 @@ const Sidebar = () => {
       <ul>
         <li>
           <a
-            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            href="https://www.linkedin.com/in/coreygallagher/"
             target="_blank"
             rel="noreferrer"
           >
@@ -85,7 +83,7 @@ const Sidebar = () => {
         </li>
         <li>
           <a
-            href="https://github.com/bobangajicsm"
+            href="https://github.com/coreygallagher1?tab=repositories"
             target="_blank"
             rel="noreferrer"
           >
@@ -96,28 +94,7 @@ const Sidebar = () => {
             />
           </a>
         </li>
-        <li>
-          <a
-            href="https://www.youtube.com/channel/UCBu5ulO4d-d47lAVybpRTkw"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FontAwesomeIcon
-              icon={faYoutube}
-              color="#4d4d4e"
-              className="anchor-icon"
-            />
-          </a>
-        </li>
-        <li>
-          <a href="skype:live:bobangajicsm" rel="noreferrer" target="_blank">
-            <FontAwesomeIcon
-              icon={faSkype}
-              color="#4d4d4e"
-              className="anchor-icon"
-            />
-          </a>
-        </li>
+
       </ul>
       <FontAwesomeIcon 
           onClick={() => setShowNav(true)}
